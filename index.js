596d54fa92fbc6b2ab92358bcafd67a7dc80013d
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2022-11-16 22:39:09 +0700

    feat: add eslint & prettier

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const { Server } = require('socket.io');
+const { Server } = require('socket.io')
 
 const io = new Server({
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
+	cors: {
+		origin: '*',
+		methods: ['GET', 'POST'],
+	},
+})
 
 io.on('connection', (socket) => {
-  console.log('socket server is running...');
-});
+	console.log('socket server is running...')
+})
 
-io.listen(8000);
+io.listen(8000)
