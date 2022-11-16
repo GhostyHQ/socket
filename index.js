6ea3aed028ae6a71799d2d8f7e09e7f23447639f
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2022-11-16 22:21:52 +0700

    feat: initial project

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Server } = require('socket.io');
+
+const io = new Server({
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('socket server is running...');
+});
+
+io.listen(8000);
